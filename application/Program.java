3791f9146c70bfbdc5e45bf82040b947eb3edc25
UeritonAraujo <ueritonaraujo160@gmail.com>
2024-08-13 22:44:08 -0300

    first commit

---
+++ application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.println("Informe os dados da conta");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Nome do titular da conta: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Saldo Inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Informe uma quantia para sacar: ");
+		double amount = sc.nextDouble();
+		
+		if (amount > acc.getWithdrawLimit()) {
+			System.out.println("Erro de saque: A quantia excede o limite de saque da conta");
+		}
+		else if (amount > acc.getBalance()) {
+			System.out.println("Erro de saque: Saldo insuficiente");
+		}
+		else {
+			acc.withdraw(amount);
+			System.out.printf("Novo saldo: $ " + String.format("%.2f", acc.getBalance()));
+		}
+		
+		
+		
+		sc.close();
+	}
+
+}
